7228392bd16231cdfe9fc58b60ad0abcb040d140
bobojonov-sobirjon <sobirbobojonov05763104@gmail.com>
2026-01-27 22:17:08 +0500

    changed

--- apps/categories/admin.py
+++ apps/categories/admin.py
@@ -6,7 +6,9 @@ from django.utils.html import mark_safe
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     def get_icon(self, obj):
-        return mark_safe(f'<img src="{obj.icon.url}" width="50" height="50" />')
+        if obj.icon:
+            return mark_safe(f'<img src="{obj.icon.url}" width="50" height="50" />')
+        return '-'
     
     get_icon.short_description = 'Иконка'
     
